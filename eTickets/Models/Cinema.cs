e3c26e187f3f96e97fb998b2d1e568215ef69ee9
Diyab Rehman <diyab.rehman@gmail.com>
2023-04-19 14:14:10 +0800

    commiting on 19apr2023

---
+++ eTickets/Models/Cinema.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Models
+{
+    public class Cinema
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Cinema Logo")]
+        public string Logo { get; set; }
+
+        [Display(Name = "Cinema Name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+
+        //Relationships
+        [NotMapped]
+        public List<Movie> Movies { get; set; }
+    }
+}
